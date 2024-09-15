6ef112ca769f9a78cbe6d0624029acefcb917d66
HadarHa22 <vsrvanaubh@gmail.com>
2024-09-15 11:15:50 +0300

    Add files via upload

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,62 @@
+package BankQ;
+
+public class BankAccount {
+	
+	private int balance;
+	private int minBalance;
+	private String owner;
+	
+	public BankAccount(int b, int m,String s){
+		this.balance = b;
+		this.minBalance = m;
+		this.owner = s;
+	}
+	
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	public int getMinBalance() {
+		return this.minBalance;
+	}
+	
+	public String getOwner() {
+		return this.owner;
+	}
+	
+	public void setBalance(int newBalance) {
+		if(newBalance > minBalance) {
+			this.balance = newBalance;
+		}
+		else{
+			System.out.println("error your new bLnce is under your limit");
+		}
+	}
+	
+	public void setOnwer(String str) {
+		this.owner = str;
+	}
+	
+	public boolean withdraw(int amount) {
+	        if (balance - amount < minBalance) {
+	            return false;}
+	        this.balance = this.balance - amount;
+	        return true;
+	}
+	
+	public void deposit(int damount){
+		this.balance = this.balance+ damount;
+	}
+	
+	public boolean transfer(BankAccount t, int tamount) {
+		if (withdraw(tamount)== false) {
+			return false;
+		}
+		t.deposit(tamount);
+		return true;
+	}
+	
+	 public String toString() {
+	        return "balance " + " of " + this.owner + ": " + balance;
+	    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package BankQ;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		BankAccount acc1 = new BankAccount(1000,-1000,"lisa");
+		BankAccount acc2 = new BankAccount(1000,-1000,"bob");
+		System.out.println(acc1.getOwner() + " balance: " + acc1.getBalance());
+		System.out.println(acc2.getOwner() + " balance: " + acc2.getBalance());
+		boolean check = acc1.transfer(acc2, 500);
+		if (check == false) {
+			System.out.println("error amount can not be transferd");
+		}
+		else {
+			System.out.println("transfer from lisa to bob success = " + check);
+		}
+		System.out.println(acc1.getOwner() + "'s balance: " + acc1.getBalance());
+		System.out.println(acc2.getOwner() + "'s balance: " + acc2.getBalance());
+		check = acc1.transfer(acc2, 1600);
+		if (check == false){
+			System.out.println("error amount can not be transferd");
+		}
+		else {
+			System.out.println("transfer from lisa to bob success = " + check);
+		} 
+		System.out.println(acc1.getOwner() + "'s balance: " + acc1.getBalance());
+		System.out.println(acc2.getOwner() + "'s balance: " + acc2.getBalance());
+	}
 }
